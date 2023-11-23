9ca4b2226224302a2d689eeba13c99027e64c6cb
lu9dce <hellocodelinux@gmail.com>
2023-11-23 09:26:52 -0300

    mejora busqueda

--- web/index.php
+++ web/index.php
@@ -199,25 +199,14 @@ function calcDistLL($lt1, $ln1, $lt2, $ln2)
 
 function locate($licrx)
 {
-    // Acceder a la variable global que contiene la información de la base de datos
     global $base;
-
-    // Obtener la longitud de la licencia proporcionada
     $z = strlen($licrx);
-
-    // Iterar a través de las posibles longitudes de licencia recortada
+    $licrx = str_replace(['\\', '/'], ['\\\\', '\\/'], $licrx);
     for ($i = $z; $i >= 1; $i--) {
-        // Obtener la parte recortada de la licencia
         $licencia_recortada = substr($licrx, 0, $i);
-
-        // Iterar a través de la base de datos para encontrar coincidencias
         foreach ($base as $resultado) {
-            // Crear una expresión regular para buscar la licencia recortada
-            $expresion_regular = '/\b ' . $licencia_recortada . ' \b/';
-
-            // Verificar si la expresión regular coincide con la licencia en el resultado actual
+            $expresion_regular = '/\b ' . $licencia_recortada . '\b/';
             if (preg_match($expresion_regular, $resultado['licencia'])) {
-                // Devolver un array con la información correspondiente
                 return array(
                     'id' => $resultado['id'],
                     'flag' => $resultado['flag'],
@@ -226,8 +215,6 @@ function locate($licrx)
             }
         }
     }
-
-    // Devolver "unknown", indicando que no se encontró una coincidencia
     return array(
         'id' => 'unknown',
         'flag' => 'unknown',
